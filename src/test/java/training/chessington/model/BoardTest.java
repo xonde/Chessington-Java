dad6ce9b5c86d9b684c20f3cf8fcbd059cd8ec58
Seth Bresnett <sgb@softwire.com>
2017-10-30 10:38:21 +0000

    Some refactoring

--- src/test/java/training/chessington/model/BoardTest.java
+++ src/test/java/training/chessington/model/BoardTest.java
@@ -34,4 +34,34 @@ public class BoardTest {
         assertThat(board.get(from)).isNull();
         assertThat(board.get(to)).isColour(PlayerColour.WHITE).isPiece(PAWN);
     }
+
+    @Test
+    public void squaresOffBoardAreOutOfRange() {
+        Board board = Board.empty();
+        assertThat(board.inRange(new Coordinates(-1, -1))).isFalse();
+        assertThat(board.inRange(new Coordinates(-1, 4))).isFalse();
+        assertThat(board.inRange(new Coordinates(8, 4))).isFalse();
+        assertThat(board.inRange(new Coordinates(4, -1))).isFalse();
+        assertThat(board.inRange(new Coordinates(4, 8))).isFalse();
+        assertThat(board.inRange(new Coordinates(8, 8))).isFalse();
+    }
+
+    @Test
+    public void squaresOnBoardAreInRange() {
+        Board board = Board.empty();
+        assertThat(board.inRange(new Coordinates(0, 0))).isTrue();
+        assertThat(board.inRange(new Coordinates(0, 7))).isTrue();
+        assertThat(board.inRange(new Coordinates(7, 0))).isTrue();
+        assertThat(board.inRange(new Coordinates(7, 7))).isTrue();
+    }
+
+    @Test
+    public void returnsCorrectlyWhenQueryingForPiece() {
+        Board board = Board.forNewGame();
+        assertThat(board.hasPieceOfColourAt(new Coordinates(0, 0), PlayerColour.BLACK)).isTrue();
+        assertThat(board.hasPieceOfColourAt(new Coordinates(0, 0), PlayerColour.WHITE)).isFalse();
+
+        assertThat(board.hasPieceOfColourAt(new Coordinates(4, 4), PlayerColour.WHITE)).isFalse();
+        assertThat(board.hasPieceOfColourAt(new Coordinates(4, 4), PlayerColour.BLACK)).isFalse();
+    }
 }
